e7339fe913c6f6c4d8331a4c16b00c576e1ab2e7
0Amrith0 <133968896+0Amrith0@users.noreply.github.com>
2025-11-08 16:34:22 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -22,10 +22,10 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Serve frontend build
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
 });
 
 app.post('/api/chat', async (req, res) => {
